a5070daf440bc3e765fc660927d86cdb1779c44d
Truong Quang Hong Phuc <hongphuc3803@gmail.com>
2023-05-17 22:19:50 +0700

    Functional Compponents

--- src/components/player.js
+++ src/components/player.js
@@ -1,79 +1,21 @@
 import React, {Component} from 'react'
+import { Players } from '../Share/ListOfPlayers'
 export default class player extends Component{
     render(){
         return(
             <div className='container'>
-                <div className='grid'>
-                    <div className='column'>
-                        <div className='card'>
-                            <img alt='' src='assets/images/cr.jpg'/>
-                            <div className="description">
-                                <h3>Cristiano Rolnaldo</h3>
-                                <p className='title'>Manchester United</p>
-                            </div>
-                            <p><button>Detail</button></p>
+            {Players.map((player)=>(
+                <div className='column'>
+                    <div className='card'>
+                        <img alt='' src={player.img}/>
+                        <div className='description'>
+                            <h3>{player.name}</h3>
+                            <p className='title'>{player.club}</p>
                         </div>
+                        <p><button>Detail</button></p>
                     </div>
-
-                    <div className='column'>
-                        <div className='card'>
-                            <img alt='' src='assets/images/kante.jpg'/>
-                            <div className='description'>
-                                <h3>Kante</h3>
-                                <p className='title'>Chelsea</p>
-                            </div>
-                            <p><button>Detail</button></p>
-                        </div>
-                    </div>
-
-                    <div className='column'>
-                        <div className='card'>
-                            <img alt='' src='assets/images/messi.jpg'/>
-                            <div className='description'>
-                                <h3>Messi</h3>
-                                <p className='title'>PSG</p>
-                            </div>
-                            <p><button>Detail</button></p>
-                        </div>
-                    </div>
-                </div>
-                <div className='grid'>
-                    <div className='column'>
-                        <div className='card'>
-                            <img alt='' src='assets/images/neymar.jpg'/>
-                            <div className='description'>
-                                <h3>Neymar</h3>
-                                <p className='title'>PSG</p>
-                            </div>
-                            <p><button>Detail</button></p>
-                        </div>
-                    </div>
-
-                    <div className='column'>
-                        <div className='card'>
-                            <img alt='' src='assets/images/kane.jpg'/>
-                            <div className='description'>
-                                <h3>Kane</h3>
-                                <p className='title'>Tottenham</p>
-                            </div>
-                            <p><button>Detail</button></p>
-                        </div>
-                    </div>
-
-                    <div className='column'>
-                        <div className='card'>
-                            <img alt='' src='assets/images/haaland.jpg'/>
-                            <div className='description'>
-                                <h3>Haaland</h3>
-                                <p className='title'>Manchester City</p>
-                            </div>
-                            <p><button>Detail</button></p>
-                        </div>
-                    </div>
-                </div>
-                <div className='footer'>
-                    <h3 className='copyright'>copyright @2022</h3>
-                </div>
+                </div>  
+            ))}  
             </div>
         )
     }
