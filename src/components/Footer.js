3e4c5d884e22cf3396908370a3beec12b259b530
Truong Quang Hong Phuc <hongphuc3803@gmail.com>
2023-05-17 17:11:20 +0700

    Components Part 2

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+export default class Footer extends Component{
+    render(){
+        return(
+            <div className='footer'>
+                <h3 className='copyright'>copyright @2022</h3>
+            </div>
+        )
+    }
+}
